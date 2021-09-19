bbe7dc690090a105940148bba113dc13cf3c87a9
Adriana <adriana.calvomatos@gmail.com>
2021-09-19 23:36:08 +0000

    implement timer

---
+++ timer1.js
@@ -0,0 +1,25 @@
+// process.stdout.write('\x07');
+
+let args = process.argv.slice(2);
+
+const timer = () => {
+  if (!args.length) {
+    return;
+  }
+
+  // sort args so beeps are in order small to big
+  args.sort((a, b) => a - b);
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] < 0 || isNaN(args[i])) {
+      continue;
+    }
+    setTimeout(() => {
+      process.stdout.write('\07');
+    }, args[i] * 1000);
+  }
+
+  return;
+};
+
+let finalTimer = timer();
